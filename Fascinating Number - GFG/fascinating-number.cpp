46f92d536eb626d45d320339ecbbceb2f5e67058
Utkarsh Mishra <anindiangeek@outlook.com>
2022-02-05 00:13:39 +0530

    Added solution - LeetHub

---
+++ Fascinating Number - GFG/fascinating-number.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	bool fascinating(int n) {
+	    string s=to_string(n);
+	    s+=to_string(n*2);
+	    s+=(to_string(n*3));
+	    sort(s.begin(),s.end());
+	    
+	    for(int i=0;i<s.length();i++)
+	    {
+	       // char c=i+49;
+	       // cout<<c<<" "<<endl;
+	        if(s[i]!=char(i+49))
+	           return false;
+	        
+	    }
+	   
+	   return true;
+	    
+	    
+	    
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.fascinating(n);
+        if (ans) {
+            cout << "Fascinating\n";
+        } else {
+            cout << "Not Fascinating\n";
+        }
+    }
+    return 0;
+}  // } Driver Code Ends
