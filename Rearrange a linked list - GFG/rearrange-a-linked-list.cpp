b332189a76af341add9246f5e5e4b9d528d679d4
Utkarsh Mishra <anindiangeek@outlook.com>
2022-02-10 15:38:34 +0530

    Added solution - LeetHub

---
+++ Rearrange a linked list - GFG/rearrange-a-linked-list.cpp
@@ -0,0 +1,106 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+ // } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    void rearrangeEvenOdd(Node *head)
+    {
+       Node * odd =new Node(-1);
+       Node * even =new Node(-1);
+       Node * o=odd, * e=even,*itr=head;
+       for(int i=1;itr;i++,itr=itr->next)
+       {
+           if(i & 1) // if odd positioned node, add it to odd
+           {
+               o->next=itr;
+               o=o->next;
+           }
+           else
+           {
+               e->next=itr;
+               e=e->next;
+           }
+       }
+       if(o)
+       o->next=even->next;
+        if(e)
+            e->next=nullptr;
+    
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,i,n,l;
+
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL;
+        struct Node *temp = head;
+        cin>>n;
+        for(i=1; i<=n; i++)
+        {
+            cin>>l;
+
+            if (head == NULL)
+            {   
+                head = temp = new Node(l); 
+                
+            }
+            else
+            {  temp->next = new Node(l);
+               temp = temp->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        while(head != NULL)
+        {
+            printf("%d ", head->data);
+            head = head->next;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
