a605f2f82ead90d80883d42d82902b1704ba50b0
Utkarsh Mishra <anindiangeek@outlook.com>
2022-02-13 16:31:19 +0530

    Added solution - LeetHub

---
+++ Total count - GFG/total-count.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int totalCount(int arr[], int n, int k) {
+        int count = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<k) count +=1;
+            else count +=(int)ceil((double)arr[i]/k);
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.totalCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
