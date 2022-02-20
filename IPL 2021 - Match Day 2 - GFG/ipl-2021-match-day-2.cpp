94d75189853ff7642d3dd342747009f89dc98b21
Utkarsh Mishra <anindiangeek@outlook.com>
2022-02-20 14:08:51 +0530

    Added solution - LeetHub

--- IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.cpp
@@ -8,11 +8,10 @@ class Solution {
   public:
     vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
         vector<int> answer((int)n-k+1,INT_MAX);
-        int start{0}, end = {start + k - 1};
-        deque<int> q;
-        for(int i=0;i<n;i++)
+        deque<int> q; // indices
+        for(int i=0;i<n;i++) // O(N)
         {
-            while(!q.empty() && q.front()<=i-k)
+            while(!q.empty() && q.front()<i-k+1)
                 q.pop_front();
                 
             while(!q.empty() && arr[q.back()]<arr[i])
@@ -20,7 +19,7 @@ class Solution {
                 
             q.push_back(i);
             
-            if(i-k+1>=0)
+            if(i-k+1 >= 0)
                 answer[i-k+1]=arr[q.front()];
         }
         return answer;
