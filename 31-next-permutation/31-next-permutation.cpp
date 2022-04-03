ef9c50737ffd6d8524e0d9390ac96d920ecdc444
Utkarsh Mishra <anindiangeek@outlook.com>
2022-04-03 13:53:55 +0530

    Time: 3 ms (87.24%), Space: 12.1 MB (75.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -5,6 +5,7 @@ public:
         
         int n = nums.size();
         int i = n - 1;
+        
         if(is_sorted(nums.rbegin(),nums.rend()))
         {
             sort(nums.begin(),nums.end());
@@ -13,15 +14,18 @@ public:
         
         if (n == 1)
             return;
+        
         while (i > 0 && nums[i] <= nums[i - 1])
             --i;
 
-        int min_idx = i;
-        int pick=nums[i-1];
+        int min_idx = i,pick=nums[i-1];
+        
         for (int j = i; j < n; j++)
             if (nums[j] < nums[min_idx] && nums[j]>pick)
                 min_idx = j;
+        
         assert(i-1>=0);
+        
         swap(nums[i - 1], nums[min_idx]);
 
         sort(nums.begin() + i, nums.end());
