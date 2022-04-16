e71de51b25d0e740260e17dfaa296177a76567ea
Utkarsh Mishra <anindiangeek@outlook.com>
2022-04-15 23:13:55 -0700

    Time: 40 ms (81.14%), Space: 34.2 MB (6.11%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root) return{};
+        vector<int> Tree;
+        stack<TreeNode *> s;
+        TreeNode * t=root;
+        while (t!=nullptr or !s.empty())
+        {
+            while(t!=nullptr)
+            {
+                s.push(t);
+                t=t->left;
+            }
+            t=s.top();
+            s.pop();
+            Tree.push_back(t->val);
+            t=t->right;
+        }
+        vector<int> prefix((int)Tree.size());
+        std::inclusive_scan(Tree.begin(), Tree.end(), prefix.begin());
+        
+        s.push(root);
+        while (!s.empty())
+        {
+            TreeNode *t = s.top();
+            s.pop();
+            int index = (lower_bound(Tree.begin(), Tree.end(), t->val) - Tree.begin());
+            t->val += (prefix[prefix.size() - 1] - prefix[index]);
+            if (t->right)
+                s.push(t->right);
+            if (t->left)
+                s.push(t->left);
+        }
+        return root;
+        
+    }
+};
