67038460dc37e72b2245f2477035e991fd769b2f
Utkarsh Mishra <anindiangeek@outlook.com>
2022-04-23 09:55:16 -0700

    Time: 11 ms (14.01%), Space: 6.6 MB (87.04%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //Skipping the problem, will do this after System Design Classes 
+    string encode(string longUrl) 
+    {
+        return longUrl;
+         //Skipping the problem, will do this after System Design Classes 
+    }
+
+    string decode(string shortUrl) 
+    {
+        return shortUrl;
+ //Skipping the problem, will do this after System Design Classes 
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
