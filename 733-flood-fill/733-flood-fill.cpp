4d80af7fb0c5e47fe66e11baedc41fa563416013
Utkarsh Mishra <anindiangeek@outlook.com>
2022-08-09 22:56:07 +0530

    Time: 7 ms (95.26%), Space: 13.9 MB (78.75%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    int x[4] = {0, 0, 1, -1};
+    int y[4] = {1, -1, 0, 0};
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color,int oldcolor)
     {
-        if(color == image[sr][sc]) return image;
-        int x[] = {0, 0, 1, -1};
-        int y[] = {1, -1, 0, 0};
-        
-        int rowsize = image.size(), colsize = image[0].size();
-        int color2 = image[sr][sc];
-        
-        queue<pair<int, int>> q;
-
-        q.push({sr, sc});
-        image[sr][sc] = color;
-
-        while (!q.empty())
+        image[sr][sc]=color;
+        for(int i=0;i<4;i++)
         {
-            pair<int, int> p = q.front();
-            q.pop();
-
-            for (int j = 0; j < 4; j++)
+            int nx = x[i]+sr, ny = y[i]+sc;
+            if(nx>=0 and ny>=0 and nx<image.size() and ny<image[0].size())
             {
-                int m = p.first + x[j], n = p.second + y[j];
-                
-                if(m>=0 and n>=0 and m<rowsize and n<colsize)
+                if(image[nx][ny]==oldcolor)
                 {
-                    if (image[m][n] == color2)
-                    {
-                        q.push({m, n});
-                        image[m][n] = color;
-                    }
+                    dfs(image,nx,ny,color, oldcolor);
                 }
             }
         }
+        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        if(color == image[sr][sc]) return image;
+        int M = image.size(), N = image[0].size();
+        int oldcolor = image[sr][sc];
+        image[sr][sc]=color;
+        dfs(image,sr,sc, color, oldcolor);
         return image;
     }
 };
