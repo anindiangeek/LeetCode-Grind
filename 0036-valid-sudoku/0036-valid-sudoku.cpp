7f5d696b0d07dccf2c67ec5e9ae88860bbc65691
Utkarsh Mishra <anindiangeek@outlook.com>
2022-11-23 13:54:49 +0530

    Time: 31 ms (81.77%), Space: 20.1 MB (33.50%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,61 +2,51 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        //checking rows only
-        for(int i=0;i<9;i++)
+        int n = board.size(), block_size = 3;
+        // checking rows only
+        for (int i = 0; i < n; i++)
         {
             unordered_set<char> Hash;
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]!='.')
+            for (int j = 0; j < n; j++)
+                if (board[i][j] != '.')
                 {
-                if(Hash.find(board[i][j])!=Hash.end())
-                    return false;
-                else
-                    Hash.insert(board[i][j]);
+                    if (Hash.find(board[i][j]) != Hash.end())
+                        return false;
+                    else
+                        Hash.insert(board[i][j]);
                 }
-            }
         }
-        
-        //checking columns
-        for(int i=0;i<9;i++)
-        {
-            unordered_set<char> Hash;
-            for(int j=0;j<9;j++)
-            {
-                if(board[j][i]!='.')
-                {
-                if(Hash.find(board[j][i])!=Hash.end())
-                    return false;
-                else
-                    Hash.insert(board[j][i]);
-                }
-            }
-        }
-        
-        // checking blocks 3x3
 
-        for(int i=0;i<9;i+=3)
+        // checking columns
+        for (int i = 0; i < n; i++)
         {
-            for(int k=0;k<9;k+=3)
+            unordered_set<char> Hash;
+            for (int j = 0; j < n; j++)
+                if (board[j][i] != '.')
+                {
+                    if (Hash.find(board[j][i]) != Hash.end())
+                        return false;
+                    else
+                        Hash.insert(board[j][i]);
+                }
+        }
+
+        // checking blocks 3x3
+        for (int i = 0; i < n; i += 3)
+            for (int k = 0; k < n; k += 3)
             {
                 unordered_set<char> Hash;
-                for(int p=k;p<k+3;p++)
-                {
-                    for(int q=i;q<i+3;q++)
-                    {
-                        if(board[p][q]!='.')
+                for (int p = k; p < k + block_size; p++)
+                    for (int q = i; q < i + block_size; q++)
+                        if (board[p][q] != '.')
                         {
-                            if(Hash.find(board[p][q])!=Hash.end())
-                                   return false;
+                            if (Hash.find(board[p][q]) != Hash.end())
+                                return false;
                             else
-                               Hash.insert(board[p][q]);
+                                Hash.insert(board[p][q]);
                         }
-                        
-                    }
-                }
             }
-        }
         return true;
+        
     }
 };
