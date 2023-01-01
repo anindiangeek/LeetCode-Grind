b0d4198bd82d94f73c1fcf33af327c8532a2db07
Utkarsh Mishra <anindiangeek@outlook.com>
2023-01-01 22:47:00 +0530

    Time: 40 ms (63.75%), Space: 13.8 MB (74.33%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordPattern(self, p: str, s: str) -> bool:
+        words, w_to_p = s.split(' '), dict()
+
+        if len(p) != len(words): return False
+        if len(set(p)) != len(set(words)): return False # for the case w = ['dog', 'cat'] and p = 'aa'
+
+        for i in range(len(words)):
+            if words[i] not in w_to_p: 
+                w_to_p[words[i]] = p[i]
+            elif w_to_p[words[i]] != p[i]: 
+                return False
+
+        return True
+
+        
